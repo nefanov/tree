331861c229f62fd69c57e059212f885c2942a07d
= <=>
2018-06-21 15:14:10 +0300

    initial tree commit

---
+++ tree.py
@@ -0,0 +1,63 @@
+class Tree: # represents a tree/subtree
+	def __init__(self, root=Node(None))
+		self.root = root
+
+#recursive dfs
+	def dfs(self, root=self.root, action=None):
+		current = root
+		# chk action
+		if len(current.children)==0:
+			return
+		else:
+			for node in current.children:
+				self.dfs(node)
+			
+#upward branch checking from current root to some global root
+
+	def upbranch(self, action=None):
+		while self.root.parent != None:
+			#chk action, thn
+			self.root = self.root.parent
+		return
+
+
+class Node:
+	def __init__(self, data, parent=None, children=[])
+		self.parent=parent
+		self.children=children
+		self.attr = data # attributes
+
+	def add_child(self, child=Node()):
+		child.parent = self
+		self.children.append(child)
+		return self
+
+	def set_parent(self, parent=Node()):
+		self.parent=parent
+		return self
+
+
+# maybe needs to set i/s attributes by separated classes
+
+
+# search routines
+
+	#recursive dfs
+        def dfs(self, action=None):
+                current = self
+                # chk action
+                if len(current.children)==0:
+                        return
+                else:
+                        for node in current.children:
+                                self.dfs(node)
+
+	#upward branch checking from current node to some global root
+
+        def upbranch(self, action=None):
+		current =  self
+                while current.parent != None:
+                        #chk action, thn
+                        current = current.parent
+                return
+
