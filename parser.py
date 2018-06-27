af0aa84aca650e76432280c8861588a7a21f0a03
636task <efanov.nn@mipt.ru>
2018-06-27 15:23:55 +0300

    add pickle loader

--- parser.py
+++ parser.py
@@ -1,11 +1,25 @@
 from pstree import *
 from tree import *
 from atriact import *
-
+import pickle
 
 class Parser:
-    def __init__(self):
-        self.pstree = get_pstree()
+    def __init__(self, pkl_fn):
+        if not pkl_fn:
+            self.pstree = get_pstree()
+        else:
+            try:
+                self.pstree=pickle.load(pkl_fn)
+            except Exception as e:
+                print('Snapshot file loading error. System is forced to get snapshot of current state')
+                self.pstree = get_pstree()
+
+    def load_from_snapshot(self, pkl_fn):
+        try:
+            self.pstree=pickle.load(pkl_fn)
+        except Exception as e:
+            print('Snapshot file loading error.')
+
 
 
     def update_pstree(self):
